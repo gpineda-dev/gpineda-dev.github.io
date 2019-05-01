266d72c68687bda5806b647a2675227cb70aaaef
PINEDA Guillaume <36532817+gpineda-dev@users.noreply.github.com>
2019-05-01 16:34:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -95,6 +95,7 @@
   <h3 class="margin">Where To Find Me?</h3><br>
   <div class="row">
     <div class="col-sm-4">
+      <?php echo $SERVER; ?>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       <img src="birds1.jpg" class="img-responsive margin" style="width:100%" alt="Image">
     </div>
